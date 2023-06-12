1539758d50df7adba7dba596a66909a8140e15b8
ayestas <ayestaskaroll@unitec.edu>
2023-06-12 00:10:43 -0600

    Cleaning Files

--- ChessboardDP/Program.cs
+++ ChessboardDP/Program.cs
@@ -3,30 +3,10 @@ using ChessboardDP.Core.Board;
 using ChessboardDP.Core.Pieces;
 using ChessboardDP.Infrastructure;
 
-Board b = new Board(8);
 
-/*
-Knight k = new Knight();
-
-Cell celda = b.setCurrentCell(2, 2);
-celda.Occupied = true;
-
-Console.WriteLine();
-
-k.Movement(b.Grid, celda);
-
-Cell newCelda = b.setCurrentCell(3, 4);
-newCelda.Occupied = true;
-
-if(k.PlaceNextMove(newCelda) == true)
-{
-    k.Movement(b.Grid, newCelda);
-    b.printBoard();
-}
-*/
 
 var engine = new PieceEngine(new FileTypeReader(), new PieceFactory(), new ConvertToPolicy());
-var p = engine.ChoosePiece();
+engine.ChoosePiece();
 
 
 Console.ReadLine();
